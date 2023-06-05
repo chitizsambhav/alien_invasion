32c8c3216dde56fbe7ce72402292237677333258
SAMBHAV <chitizsambhav23@gmail.com>
2023-06-05 10:59:19 +0530

    Added Alien fleet renewal and collision Feature

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,21 +2,26 @@ import pygame
 from ship import Ship
 import game_functions as gf
 from settings import Settings
-
-ai_settings = Settings()
+from pygame.sprite import Group
+from dimension import Dimension
 
 
 def run_game():
     pygame.init()
-    screen_info = pygame.display.Info()
-    screen = pygame.display.set_mode((screen_info.current_w, screen_info.current_h))
+    dimensions = Dimension()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((dimensions.screen_width, dimensions.screen_height))
     ship = Ship(ai_settings, screen)
     pygame.display.set_caption('Alien Invasion')
-
+    bullets = Group()
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, aliens, ship)
     while True:
-        gf.check_event(ship)
+        gf.check_event(ai_settings, ship, screen, bullets)
         ship.update()
-        gf.update_screen(ai_settings, ship, screen)
+        gf.update_bullets(bullets, aliens, ai_settings, screen, ship)
+        gf.update_aliens(aliens, ai_settings)
+        gf.update_screen(ai_settings, ship, aliens, screen, bullets)
 
 
 run_game()
